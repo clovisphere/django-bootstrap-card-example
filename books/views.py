413b2fb6759426e4530c766972cc835c20e39fb8
clov <clovis.mugaruka@gmail.com>
2019-10-23 21:22:59 +0300

    Update views.py

--- books/views.py
+++ books/views.py
@@ -6,12 +6,12 @@ def index(request):
     books = Book.objects.order_by('id')
     # -- transform data before using it --
     data = books.values()
-    # let's do something fancy -- this should be changed
+    # let's do something fancy -- this should be changed, we'd be able to have 2-3 books on each card.
     context = {
         'tags': [
-            {'name': 'Programming', 'books': data[:2]},
-            {'name': 'Programming', 'books': data[2:4]},
-            {'name': 'Programming', 'books': data[4:]},
+            {'name': 'Programming', 'books': data},
+            {'name': 'Programming', 'books': data},
+            {'name': 'Programming', 'books': data},
         ]
     }
     return render(request, 'books/index.html', context)
